023c3ab679a5860b1a32294b40229c4bd07af1f4
Ryan Drew <learnitall0@gmail.com>
2021-04-28 11:58:23 -0600

    Add background image and preload assets

--- app.js
+++ app.js
@@ -3,13 +3,19 @@
 // Made with <3
 // P.S. Opal is a cutie
 
+var width = 800;
+var height = 600;
+var background_color = "0xe6ffe6"
 
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: width,
+    height: height,
     physics: {
-        default: 'arcade'
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
     },
     scene: {
         preload: preload,
@@ -18,14 +24,19 @@ var config = {
     }
 };
 
+var game = new Phaser.Game(config);
+
 function preload()
 {
-
+    this.load.image('tardigrade', 'assets/tardigrade.svg');
+    this.load.image('nematode', 'assets/worm.svg');
+    this.load.image('food', 'assets/leaf.svg');
+    this.load.image('amoeba', 'assets/blob.svg');
 }
 
 function create()
 {
-
+    this.add.rectangle(width / 2, height / 2, width, height, background_color);
 }
 
 function update()
