c4d0658a201c568fed5126fd8eb5305a8f6c60aa
Ryan Drew <learnitall0@gmail.com>
2021-04-27 18:19:30 -0600

    Add initial structure to app.ys

---
+++ app.js
@@ -0,0 +1,34 @@
+// Tardirave!!
+// Ryan Drew, 2020
+// Made with <3
+// P.S. Opal is a cutie
+
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade'
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+function preload()
+{
+
+}
+
+function create()
+{
+
+}
+
+function update()
+{
+
+}
